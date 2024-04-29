2cd61aa321b5759c6cc89189058e014cf6259199
usrachap <64814146+usrachap@users.noreply.github.com>
2024-04-28 21:33:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +' '
-
-        #st.write(ingredients_string)
-    
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe( data = fruityvice_response.json(), use_container_width = True)
+       
     my_insert_stmt = """insert into smoothies.public.orders(ingredients,name_on_order)
         values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
     
@@ -42,9 +42,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is Ordered !') 
 
-# New Section to display fruitvice nutrition information
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe( data = fruityvice_response.json(), use_container_width = True)
-        
-        
