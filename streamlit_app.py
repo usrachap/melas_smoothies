d24eb0ded8da80fb296fcfd497d4ed6d85af4562
usrachap <64814146+usrachap@users.noreply.github.com>
2024-04-28 21:22:54 -0500

    Update streamlit_app.py
    added fruitvey response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -39,5 +40,10 @@ if ingredients_list:
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        
         st.success('Your Smoothie is Ordered !') 
+   
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
+        
+        
